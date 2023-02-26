43398c7ebad88a7941ba5d1e25b9a902b80db5b8
Diego Villan <dnvx81@outlook.com>
2023-02-26 01:13:09 -0300

    subir proyecto

---
+++ src/conversor/Conversor.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package conversor;
+
+
+
+/**
+ *
+ * @author dnvx8
+ */
+public class Conversor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
